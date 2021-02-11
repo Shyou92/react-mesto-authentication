a40522f4bbac2516af9fa0f88d0364c7ddcff499
Alexander Vitchinov <vitalex92@yandex.ru>
2021-02-11 22:56:05 +0300

    Register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,40 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Register() {
+  const [data, setData] = useState({
+    userEmail: '',
+    userPassword: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (data.userPassword) {
+      let { userEmail, userPassword } = data;
+      console.log({ userEmail, userPassword });
+    }
+  };
   return (
     <div className='register'>
       <h2 className='register__heading'>Регистрация</h2>
-      <form className='register__form'>
+      <form className='register__form' onSubmit={handleSubmit}>
         <input
           className='popup__input popup__input_dark'
           type='email'
           name='userEmail'
           placeholder='Email'
+          value={data.userEmail}
+          onChange={handleChange}
           noValidate
         />
         <input
@@ -17,6 +42,8 @@ function Register() {
           type='password'
           name='userPassword'
           placeholder='Пароль'
+          value={data.userPassword}
+          onChange={handleChange}
           noValidate
         />
         <button className='register__form-submit' type='submit'>
